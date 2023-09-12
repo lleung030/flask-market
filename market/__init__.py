58d91c54da8a61f1c55c5b86c237a46dc3424661
lleung030 <lucasleung2593@gmail.com>
2023-09-12 14:37:26 -0700

    register, forms, base

--- market/__init__.py
+++ market/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+app.config['SECRET_KEY'] = '91b40d208c37c1f100d88c85'
 db = SQLAlchemy(app)
 
 

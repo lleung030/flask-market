fca30bebf7966c6d10335e5d6f5e5aaf613851e0
lleung030 <lucasleung2593@gmail.com>
2023-09-13 12:56:55 -0700

    logout

--- market/__init__.py
+++ market/__init__.py
@@ -9,4 +9,6 @@ app.config['SECRET_KEY'] = '91b40d208c37c1f100d88c85'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
+login_manager.login_view = 'login_page'
+login_manager.login_message_category = 'info'
 from market import routes
